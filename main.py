2f5fbbdea3809e87cf7179c7d6e1c874dfe06958
Kqzz <48300938+Kqzz@users.noreply.github.com>
2022-09-05 19:58:55 +0000

    parse dates and titles

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from typing import Dict
-import requests
+from typing import List
 import re
+import requests
+from datetime import datetime
+import json
+
+find_events = re.compile('(?<=imp_calendar = ).*(?=;<\/script>)')
+
 
 def log(m: str):
     print(f"[*] {m}")
 
-def get_events(region: str) -> typing.Dict|None:
+
+def get_events(region: str) -> List | None:
     url = f"https://fortnitetracker.com/events?region={region}"
     log(f"GET: {url}")
 
@@ -13,8 +19,21 @@ def get_events(region: str) -> typing.Dict|None:
 
     if req.status_code != 200:
         log(f"failed: {req.status_code}")
-        return None 
-    
-    log(req.content)
+        return None
 
-get_events("NAE")
+    page = req.text.replace("\r\n", "").replace("00.001", "00")
+    events_txt = find_events.findall(page, re.DOTALL)
+
+    e = json.loads(events_txt[0])
+
+    return e
+
+
+events = get_events("NAE")
+
+for event in events:
+    data = event.get("customData")
+    date = datetime.strptime(data["windows"][0]["beginTime"], "%Y-%m-%dT%H:%M:%S+00:00")
+    title = data["title"]
+
+    log(f"{title}: {date}")
