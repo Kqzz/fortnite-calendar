525ff5e28bc176daa4679f5ad9750c361450d726
Kqzz <48300938+Kqzz@users.noreply.github.com>
2022-09-05 15:24:40 -0400

    Update it yes

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# main.py
+from typing import Dict
+import requests
+import re
+
+def log(m: str):
+    print(f"[*] {m}")
+
+def get_events(region: str) -> typing.Dict|None:
+    url = f"https://fortnitetracker.com/events?region={region}"
+    log(f"GET: {url}")
+
+    req = requests.get(url)
+
+    if req.status_code != 200:
+        log(f"failed: {req.status_code}")
+        return None 
+    
+    log(req.content)
+
+get_events("NAE")
